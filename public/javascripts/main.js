2ef9b0f6eaf57fea14c229f91225b7c71c8a20dc
Stefan Francis <Stefan.Francis@nyulangone.org>
2019-09-30 20:51:50 +0530

    Modifications

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,21 +1,22 @@
-jQuery(function($) {
-  $("#menu-button").on("click", function() {
+jQuery(function ($) {
+  var date;
+  $("#menu-button").on("click", function () {
     $("#main-menu").toggleClass("menu-visible");
     $(this).toggleClass("menu-visible");
   });
 
   var $form = $('#add-row-form');
 
-  $(".add-row-button").on("click", function() {
-    $form.slideToggle(function(){
+  $(".add-row-button").on("click", function () {
+    $form.slideToggle(function () {
       $(!$form.hasClass("form-hidden"))
-        resetForm($form);
+      resetForm($form);
       $form.toggleClass("form-hidden");
       $(".add-row-button").toggleClass("open-form");
-    });  
+    });
   });
 
-  $("#dataTable").on("click", "th i", function() {
+  $("#dataTable").on("click", "th i", function () {
     var sortBy = $(this).parent().data("sort"), nextUrl,
       url = window.location.pathname.split("/");
     if (url[2].indexOf("sortBy") > -1) {
@@ -25,31 +26,31 @@ jQuery(function($) {
     }
     nextUrl = url.join("/") + window.location.search;
 
-    window.location = nextUrl
+    // window.location = nextUrl
 
     // sortTable(index);
   });
 
-  $('#add-row-form :input:not([type="submit"])').focusin(function() {
-    $('#add-row-form :input:not([type="submit"])').removeClass("error");
+  $('#add-row-form :input:not([type="submit"])').focusin(function () {
+    $(this).removeClass("error");
   });
 
-  $("#search").on("keyup", function(e) {
-    if(e.keyCode === 13){
-      searchTable();
+  $("#search").on("keyup", function (e) {
+    if (e.keyCode === 13) {
+      searchTable(date);
     }
   });
 
-  $form.on( "submit", function(event) {
+  $form.on("submit", function (event) {
     event.preventDefault();
-    if(validateForm()) return;
-    var form = $( this ),
-        url = form.attr( 'action' );
+    if (validateForm()) return;
+    var form = $(this),
+      url = form.attr('action');
     $.ajax({
       type: "POST",
       url: url,
       data: form.serialize(),
-      success: function( response ) {
+      success: function (response) {
         alert("Success");
         resetForm($form);
         $(addNewRow(response)).insertAfter("#dataTable thead");
@@ -58,71 +59,72 @@ jQuery(function($) {
   });
   $('input[name="daterange"]').daterangepicker({
     opens: 'left'
-  }, function(start, end, label) {
+  }, function (start, end, label) {
 
     var date1 = start.format('YYYY-MM').split('-'),
       date2 = end.format('YYYY-MM').split('-'),
       data = {
-        x:new Date(date1[0], date1[1]-1).getTime(),
-        y:new Date(date2[0], date2[1]-1).getTime()
+        x: new Date(date1[0], date1[1] - 1).getTime(),
+        y: new Date(date2[0], date2[1] - 1).getTime()
       };
-    window.location = "/vincent-de-paul/get_date_filter?x="+data.x+"&y="+data.y
+    date = data
+    // window.location = "/vincent-de-paul/get_date_filter?s=" + data.x + "&e=" + data.y
   });
   var pageParam = window.location.href.split('/').slice(-1)[0],
     pageNum = parseInt(pageParam.substring(pageParam.indexOf("page=") + 5)),
-    startPage = pageNum > 0 && pageNum <= Math.ceil($("#total").val()/5) ? pageNum : 1;
+    startPage = pageNum > 0 && pageNum <= Math.ceil($("#total").val() / 5) ? pageNum : 1;
   $('#pagination').twbsPagination({
-      totalPages: Math.ceil($("#total").val()/5),
-      visiblePages: 5,
-      startPage: startPage,
-      initiateStartPageClick: false,
-      onPageClick: changeToPage
+    totalPages: Math.ceil($("#total").val() / 5),
+    visiblePages: 5,
+    startPage: startPage,
+    initiateStartPageClick: false,
+    onPageClick: changeToPage
   });
   searchHighlighting();
 });
 
-Array.prototype.insert = function ( index, item ) {
-    this.splice( index, 0, item );
+Array.prototype.insert = function (index, item) {
+  this.splice(index, 0, item);
 };
 
-var changeToPage = function(event, pageNum) {
-    event.preventDefault();
-    var data = {
-      pageNum: pageNum,
-      pageSize: 5
-    };
-    var totalPath = window.location.pathname + window.location.search,
-      page = totalPath.split("/").slice(-1)[0], nextUrl;
-    if(page.indexOf("page=") > -1) {
-      nextUrl = totalPath.replace(totalPath.substring(totalPath.indexOf("page=")), "page=" +data.pageNum);
-    } else {
-      nextUrl = totalPath + "page=" + data.pageNum;
-    }
-    window.location = nextUrl
+var changeToPage = function (event, pageNum) {
+  event.preventDefault();
+  var data = {
+    pageNum: pageNum,
+    pageSize: 5
+  };
+  var totalPath = window.location.pathname + window.location.search,
+    page = totalPath.split("/").slice(-1)[0], nextUrl;
+  if (page.indexOf("page=") > -1) {
+    nextUrl = totalPath.replace(totalPath.substring(totalPath.indexOf("page=")), "page=" + data.pageNum);
+  } else {
+    nextUrl = totalPath + "page=" + data.pageNum;
   }
+  window.location = nextUrl
+}
 
-var addNewRow = function(data) {
+var addNewRow = function (data) {
 
-  var template = 
-    "<tr data-node-id='<%= data._id %>'>"+
-      "<td><%= data.date.slice(0,7) %></td>"+
-      "<td><%= data.voucher %></td>"+
-      "<td><%= data.details %></td>"+
-      "<td><%= data.credit %></td>"+
-      "<td><%= data.debit %></td>"+
-      "<td><%= data.bank_credit_debit %></td>"+
-      "<td><%= data.cash_in_bank %></td>"+
-      "<td><%= data.cash_in_hand %></td>"+
+  var template =
+    "<tr data-node-id='<%= data._id %>'>" +
+    "<td><%= data.date.slice(0,7) %></td>" +
+    "<td><%= data.voucher %></td>" +
+    "<td><%= data.details %></td>" +
+    "<td><%= data.credit %></td>" +
+    "<td><%= data.debit %></td>" +
+    "<td><%= data.bank_credit_debit %></td>" +
+    "<td><%= data.cash_in_bank %></td>" +
+    "<td><%= data.cash_in_hand %></td>" +
     "</tr>",
-    html = ejs.render(template, {data: data});
-    return html;
+    html = ejs.render(template, { data: data });
+  return html;
 
 }
 
-var validateForm = function() {
+var validateForm = function () {
   var $form = $('#add-row-form :input:not([type="submit"])'),
     isEmpty = false;
-  $form.each(function(index,item){
+  $form.each(function (index, item) {
     var value = $(item).val();
     if (!value) {
       $(item).addClass("error");
@@ -132,37 +134,37 @@ var validateForm = function() {
   return isEmpty;
 }
 
-var resetForm = function($form) {
+var resetForm = function ($form) {
   $form[0].reset();
   $form = $('#add-row-form :input:not([type="submit"])');
-  $form.each(function(index,item){
+  $form.each(function (index, item) {
     $(item).removeClass("error");
   });
 }
 
-var searchTable = function() {
+var searchTable = function (date) {
   var input = document.getElementById("search");
 
-  window.location = "/vincent-de-paul/" + (input.value == "" ? "" : "search=" + input.value + "&page=1");
+  window.location = "/vincent-de-paul/" + (input.value == "" ? "" : "search=" + input.value + '"/get_date_filter?s=" + date.x + "&e=" + date.y"' + "&page=1");
 }
 
 // removes highlighting by replacing each em tag within the specified elements with it's content
-var removeHighlighting = function(highlightedElements) {
-    highlightedElements.each(function(){
-        var element = $(this);
-        element.replaceWith(element.html());
-    })
+var removeHighlighting = function (highlightedElements) {
+  highlightedElements.each(function () {
+    var element = $(this);
+    element.replaceWith(element.html());
+  })
 }
 
 // add highlighting by wrapping the matched text into an em tag, replacing the current elements, html value with it
-var addHighlighting = function(element, textToHighlight, index) {
-    var text = element.text(),
-      newText = text.substring(0,index) + '<em>' + text.substring(index,index+textToHighlight.length) + '</em>' + text.substring(index+textToHighlight.length,text.length);
+var addHighlighting = function (element, textToHighlight, index) {
+  var text = element.text(),
+    newText = text.substring(0, index) + '<em>' + text.substring(index, index + textToHighlight.length) + '</em>' + text.substring(index + textToHighlight.length, text.length);
 
-    element.html(newText);
+  element.html(newText);
 }
 
-var searchHighlighting = function() {
+var searchHighlighting = function () {
   var input, filter, table, tr, td, i, $tdElement, value, matchedIndex;
   input = document.getElementById("search");
   filter = input.value.toUpperCase();
@@ -170,13 +172,13 @@ var searchHighlighting = function() {
   tr = table.getElementsByTagName("TR");
 
   removeHighlighting($("#dataTable tr em"));
-  $(tr).each(function(index) {
-      $row = $(this);
+  $(tr).each(function (index) {
+    $row = $(this);
 
-      var $tdElement = $row.find("td:nth-child(3)");
-      var value = $tdElement.text();
-      var matchedIndex = value.toUpperCase().indexOf(filter);
+    var $tdElement = $row.find("td:nth-child(3)");
+    var value = $tdElement.text();
+    var matchedIndex = value.toUpperCase().indexOf(filter);
 
-      addHighlighting($tdElement, filter, matchedIndex);
+    addHighlighting($tdElement, filter, matchedIndex);
   });
 }
--- routes/vincent-de-paul.js
+++ routes/vincent-de-paul.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var router = express.Router();
 var ObjectId = require('mongodb').ObjectId;
-var IncomeExpenditure  = require('../models/IncomeExpenditure');
+var IncomeExpenditure = require('../models/IncomeExpenditure');
 var handleError = require('../helpers/handleError');
 
-var loadPage = function(req, res, next) {
+var loadPage = function (req, res, next) {
 	var pageSize = req.params.pageSize != null ? Number(req.params.pageSize) : 5;
 	var pageNum = req.params.page != null ? Number(req.params.page) : 1;
 	var sortBy = req.params.sortBy != null ? req.params.sortBy : "_id";
@@ -12,21 +12,21 @@ var loadPage = function(req, res, next) {
 	var total, sort = {};
 	sort[sortBy] = -1;
 	console.log(req.params);
-	IncomeExpenditure.count(function(err, data){
+	IncomeExpenditure.count(function (err, data) {
 		total = data;
-		if (pageNum > total/pageSize + 1) {
+		if (pageNum > total / pageSize + 1) {
 			res.redirect("/vincent-de-paul");
 			return;
 		}
-		IncomeExpenditure.find().skip(skips).limit(pageSize).sort(sort).exec(function(err, table) {
-	    if (err) return handleError(err);
-	    table.total = total;
+		IncomeExpenditure.find().skip(skips).limit(pageSize).sort(sort).exec(function (err, table) {
+			if (err) return handleError(err);
+			table.total = total;
 			res.render('vincent-de-paul', { title: 'Society of Saint Vincent de Paul', data: table, searchQuery: "", total: total });
 		});
 	});
 }
 
-var searchPage = function(req, res, next) {
+var searchPage = function (req, res, next) {
 	var pageSize = req.query.pageSize != null ? Number(req.query.pageSize) : 5;
 	var pageNum = req.query.page != null ? Number(req.query.page) : 1;
 	var sortBy = req.params.sortBy != null ? req.params.sortBy : "_id";
@@ -34,40 +34,40 @@ var searchPage = function(req, res, next) {
 	req.params.value = req.params.value == undefined ? "" : req.params.value;
 	var total, sort = {};
 	sort[sortBy] = -1;
-	IncomeExpenditure.find({details: {$regex : req.params.value} }).count().exec(function(err, data){
+	IncomeExpenditure.find({ details: { $regex: req.params.value } }).count().exec(function (err, data) {
 		total = data;
-	  IncomeExpenditure.find({details: {$regex : req.params.value} }).skip(skips).limit(pageSize).sort(sort).limit(5).exec(function (err, table) {
-	    if (err) return handleError(err);
-		  res.render('vincent-de-paul', { title: 'Society of Saint Vincent de Paul', data: table, searchQuery: req.params.value, total: total });
-	  });
+		IncomeExpenditure.find({ details: { $regex: req.params.value } }).skip(skips).limit(pageSize).sort(sort).limit(5).exec(function (err, table) {
+			if (err) return handleError(err);
+			res.render('vincent-de-paul', { title: 'Society of Saint Vincent de Paul', data: table, searchQuery: req.params.value, total: total });
+		});
 	});
 }
 
-var dateFilterPage = function(req, res, next) {
-  var data = {x: req.query.x, y: req.query.y}, total;
-  var pageSize = req.query.pageSize != null ? Number(req.query.pageSize) : 5;
-  var pageNum = req.query.pageNum != null ? Number(req.query.pageNum) : 1;
-  var skips = pageSize * (pageNum - 1);
-  IncomeExpenditure.find({
-    date: {
-        $gte: data.x,
-        $lt: data.y
-    }
-  })
-  .count(function(err, data){
-    total = data;
-    IncomeExpenditure.find({
-      date: {
-          $gte: data.x,
-          $lt: data.y
-      }
-    })
-    .skip(skips).limit(pageSize).sort({_id: -1}).exec(function(err, table) {
-      if (err) return handleError(err);
-      table.total = total;
-      res.render('vincent-de-paul', { title: 'Society of Saint Vincent de Paul', data: table, searchQuery: "", total: total });
-    });
-  });
+var dateFilterPage = function (req, res, next) {
+	var data = { start: req.query.s, end: req.query.e }, total;
+	var pageSize = req.query.pageSize != null ? Number(req.query.pageSize) : 5;
+	var pageNum = req.query.pageNum != null ? Number(req.query.pageNum) : 1;
+	var skips = pageSize * (pageNum - 1);
+	IncomeExpenditure.find({
+		date: {
+			$gte: data.start,
+			$lt: data.end
+		}
+	})
+		.count(function (err, data) {
+			total = data;
+			IncomeExpenditure.find({
+				date: {
+					$gte: data.start,
+					$lt: data.end
+				}
+			})
+				.skip(skips).limit(pageSize).sort({ _id: -1 }).exec(function (err, table) {
+					if (err) return handleError(err);
+					table.total = total;
+					res.render('vincent-de-paul', { title: 'Society of Saint Vincent de Paul', data: table, searchQuery: "", total: total });
+				});
+		});
 
 
 }
@@ -83,6 +83,6 @@ router.get('/sortBy=:sortBy&search=:value&page=:page', searchPage);
 
 /* GET Date Filter page. */
 router.get('/get_date_filter/:x?/:y?', dateFilterPage);
-router.get('/get_date_filter/sortBy=:sortBy?/:x?&:y?', dateFilterPage);
+router.get('/get_date_filter/sortBy=:sortBy?/:s?&:e?', dateFilterPage);
 
 module.exports = router;
